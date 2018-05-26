1a43a54582cddf04a0d51d741f3351d0efa2cad1
Noah Rosamilia <ivoahivoah@gmail.com>
2018-05-25 20:45:43 -0400

    Add gradient notes

--- pyano.py
+++ pyano.py
@@ -6,40 +6,16 @@ import pygame.time
 
 from pygame.locals import *
 
+from util import *
+
 TIME_SCALE = 5
 
-whites = [0, 2, 4, 5, 7, 9, 11]
-blacks = [1, 3, 6, 8, 10]
+channel_colors = [
+    ((0x5a, 0x9f, 0xd4), (0x30, 0x69, 0x98)),
+    ((0xff, 0xd4, 0x3b), (0xff, 0xe8, 0x73))
+]
 
 pygame.init()
-
-def border_box(size, border, col1=(255, 255, 255), col2=(0, 0, 0)):
-    s = pygame.Surface(size)
-    s.fill(col2)
-    s.fill(col1, (border, border, size[0] - 2*border, size[1] - 2*border))
-    return s
-
-def key_pos(key):
-    if key in whites:
-        return whites.index(key)/len(whites)
-    else:
-        return key/12
-
-def key_width(key):
-    if key in whites:
-        return 1/len(whites)
-    else:
-        return 1/12
-
-def draw_octave(size, highlights=[]):
-    s = pygame.Surface(size)
-    for key in whites:
-        key_surf = border_box((size[0]*key_width(key), size[1]), 5, col1=(255, 255, 224) if key in highlights else (255, 255, 255))
-        s.blit(key_surf, (size[0]*key_pos(key), 0))
-    for key in blacks:
-        key_surf = border_box((size[0]*key_width(key), size[1]*2/3), 5, col1=(255, 255, 224) if key in highlights else (0, 0, 0))
-        s.blit(key_surf, (size[0]*key_pos(key), 0))
-    return s
 
 window = pygame.display.set_mode((640, 480))
 keyb = mido.open_input()
@@ -79,23 +55,23 @@ try:
         if msg:
             if msg.type == 'note_on' and msg.velocity > 0:
                 notes_played.append({
-                    'note': msg.note - 60,
+                    'note': msg.note,
                     'channel': msg.channel,
                     'start': pygame.time.get_ticks(),
                     'stop': None
                 })
             elif msg.type == 'note_off' or msg.type == 'note_on' and msg.velocity == 0:
                 for note in notes_played:
-                    if note['note'] == msg.note - 60 and note['stop'] is None:
+                    if note['note'] == msg.note and note['stop'] is None:
                         note['stop'] = pygame.time.get_ticks()
 
         window.fill((0, 255, 255))
-        window.blit(draw_octave((640, 480/3), [note['note'] for note in notes_played if note['stop'] is None]), (0, 480*2/3))
+        window.blit(draw_octave((640, 480/3), 5, [note['note'] for note in notes_played if note['stop'] is None]), (0, 480*2/3))
 
         hit_list = []
         for note in sorted(notes_played, key=lambda n: n['start']):
             if note['stop'] is not None and note['stop'] < (pygame.time.get_ticks() - (480*2/3)*TIME_SCALE): hit_list.append(note)
-            s = border_box((key_width(note['note'])*640, ((note['stop'] or pygame.time.get_ticks()) - note['start'])/TIME_SCALE), 5, col1=(255, 255, 224))
+            s = border_box((key_width(note['note'])*640, ((note['stop'] or pygame.time.get_ticks()) - note['start'])/TIME_SCALE), 5, col1=channel_colors[note['channel']])
             window.blit(s, (key_pos(note['note'])*640, 480*2/3 + (note['start'] - pygame.time.get_ticks())/TIME_SCALE))
 
         for note in hit_list:
---
+++ util.py
@@ -0,0 +1,52 @@
+import pygame
+
+lerp = lambda a, b, r: a + (b - a)*r
+
+def fill_gradient(surface, c1, c2, rect=None):
+    if rect is None: rect = surface.get_rect()
+
+    for column in range(rect.width):
+        c = (
+            lerp(c1[0], c2[0], column/rect.width),
+            lerp(c1[1], c2[1], column/rect.width),
+            lerp(c1[2], c2[2], column/rect.width)
+        )
+        pygame.draw.line(surface, c, (rect.left + column, rect.top), (rect.left + column, rect.bottom))
+
+def border_box(size, border, col1, col2=(0, 0, 0)):
+    s = pygame.Surface(size)
+    s.fill(col2)
+    if len(col1) == 2:
+        fill_gradient(s, col1[0], col1[1], pygame.Rect(border, border, size[0] - 2*border, size[1] - 2*border))
+    elif len(col1) == 3:
+        s.fill(col1, (border, border, size[0] - 2*border, size[1] - 2*border))
+    else:
+        raise ValueError('col1 must be a color or tuple containing 2 colors')
+    return s
+
+whites = [0, 2, 4, 5, 7, 9, 11]
+blacks = [1, 3, 6, 8, 10]
+
+def key_pos(key):
+    key %= 12
+    if key in whites:
+        return whites.index(key)/len(whites)
+    else:
+        return key/12
+
+def key_width(key):
+    key %= 12
+    if key in whites:
+        return 1/len(whites)
+    else:
+        return 1/12
+
+def draw_octave(size, octave, highlights=[]):
+    s = pygame.Surface(size)
+    for key in whites:
+        key_surf = border_box((size[0]*key_width(key), size[1]), 5, col1=(255, 255, 224) if key + octave*12 in highlights else (255, 255, 255))
+        s.blit(key_surf, (size[0]*key_pos(key), 0))
+    for key in blacks:
+        key_surf = border_box((size[0]*key_width(key), size[1]*2/3), 5, col1=(255, 255, 224) if key + octave*12 in highlights else (0, 0, 0))
+        s.blit(key_surf, (size[0]*key_pos(key), 0))
+    return s
