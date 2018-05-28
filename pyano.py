ef7d3a71d9959339d311018c40350353698e6df9
Noah Rosamilia <ivoahivoah@gmail.com>
2018-05-28 10:09:11 -0400

    Remove unused code

--- pyano.py
+++ pyano.py
@@ -15,7 +15,6 @@ HEIGHT = 480
 pygame.init()
 
 window = pygame.display.set_mode((WIDTH, HEIGHT))
-#keyb = mido.open_input()
 synth = mido.open_output()
 clock = pygame.time.Clock()
 font = pygame.font.SysFont('Helvetica', 12)
@@ -43,8 +42,6 @@ for msg in midi_file:
 
 song = sorted(song, key=lambda note: note['start'])
 
-notes_played = []
-
 try:
     running = True
     while running:
@@ -52,30 +49,7 @@ try:
             if (event.type == KEYDOWN and event.mod == 1024 and event.key == 113) or event.type == QUIT:
                 running = False
 
-        msg = None #keyb.poll()
-        if msg:
-            if msg.type == 'note_on' and msg.velocity > 0:
-                notes_played.append({
-                    'note': msg.note,
-                    'channel': msg.channel,
-                    'start': pygame.time.get_ticks(),
-                    'stop': None
-                })
-            elif msg.type == 'note_off' or msg.type == 'note_on' and msg.velocity == 0:
-                for note in notes_played:
-                    if note['note'] == msg.note and note['stop'] is None:
-                        note['stop'] = pygame.time.get_ticks()
-
         window.fill((0, 255, 255))
-
-        hit_list = []
-        for note in sorted(notes_played, key=lambda n: n['start']):
-            if note['stop'] is not None and note['stop'] < (pygame.time.get_ticks() - (HEIGHT*2/3)*TIME_SCALE): hit_list.append(note)
-            s = border_box((key_width(note['note'])*WIDTH, ((note['stop'] or pygame.time.get_ticks()) - note['start'])/TIME_SCALE), 5, col1=channel_colors[note['channel']])
-            window.blit(s, (key_pos(note['note'])*WIDTH, HEIGHT*2/3 + (note['start'] - pygame.time.get_ticks())/TIME_SCALE))
-
-        for note in hit_list:
-            notes_played.remove(note)
 
         highlight = {}
         for note in song:
